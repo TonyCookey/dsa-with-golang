a2f4c020905e3aa2f94e965d4c283559ee1e2a55
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-03 08:07:08 +0100

    Solved: Two sum Problem using three approaches 1. Brute Force - quadratic time 2. Using Maps - linear time 2. Using Maps(Cleaner) - linear time

---
+++ problem-solving/two-sum/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	sliceOfInts := []int{7, 9, 5, 2, 1}
+	sum := 8
+	fmt.Println(twoSum(sliceOfInts, sum))
+	fmt.Println(twoSumCleaner(sliceOfInts, sum))
+	fmt.Println(usingBruteForce(sliceOfInts, sum))
+}
+
+// Solving twoSum using maps - a more efficient approach
+func twoSum(nums []int, target int) []int {
+	// Time Complexity: O(n)
+	// get the indices of the pair that sum up to target
+	complements := map[int]int{}
+
+	for i := 0; i < len(nums); i++ {
+		if complements[nums[i]] != 0 {
+			return []int{complements[nums[i]] - 1, i}
+		}
+		complements[target-nums[i]] = i + 1
+	}
+	return nil
+}
+
+// Solving twoSum using maps/hash maps - a more efficient and cleaner approach
+func twoSumCleaner(nums []int, target int) []int {
+	// Time Complexity: O(n)
+	// get the indices of the pair that sum up to target
+	complements := map[int]int{}
+	for index, value := range nums {
+		if _, ok := complements[value]; ok {
+			return []int{complements[value], index}
+		}
+		complements[target-value] = index
+	}
+	return nil
+}
+
+// Solving twoSum using the brute force approach
+func usingBruteForce(nums []int, target int) []int {
+	//Time Complexity : O(n^2)
+	for i := 0; i < len(nums); i++ {
+		for j := 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
