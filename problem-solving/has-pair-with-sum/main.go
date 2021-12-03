0742a8c1c3350cf32b4a19e2b3d20a4120fd0480
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-03 08:05:31 +0100

    updated the haPairSum solution

--- problem-solving/has-pair-with-sum/main.go
+++ problem-solving/has-pair-with-sum/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	sliceOfInts := []int{7, 4, 5, 2, 3, 4, 1, 8, 6}
-	sum := 10
+	sliceOfInts := []int{7, 9, 5, 2, 1}
+	sum := 8
 	fmt.Println(usingBruteForce(sliceOfInts, sum))
 	fmt.Println(hasPairWithSum(sliceOfInts, sum))
 }
@@ -15,7 +15,6 @@ func hasPairWithSum(slice []int, sum int) bool {
 	// used to store the complements - (sum - slice[i]), which is what is needed for a pair sum
 	complements := map[int]bool{}
 	for i := 0; i < len(slice); i++ {
-		fmt.Println("complements", complements)
 		if complements[slice[i]] {
 			return true
 		}
@@ -26,6 +25,7 @@ func hasPairWithSum(slice []int, sum int) bool {
 
 // Solving hasPairWithSum using the brute force approach
 func usingBruteForce(slice []int, sum int) bool {
+
 	//Time Complexity : O(n^2)
 	for i := 0; i < len(slice); i++ {
 		for j := 0; j < len(slice); j++ {
