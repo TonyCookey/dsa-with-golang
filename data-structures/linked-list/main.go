d8e636983eabdbf0d9d4a4b18112becb27321eb6
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-11 21:25:59 +0100

    LinkedList: worked on the append method

--- data-structures/linked-list/main.go
+++ data-structures/linked-list/main.go
@@ -12,3 +12,26 @@ type LinkedList struct {
 	length int
 	tail   *Node
 }
+
+// append - add a node to the end of the linked list
+func (l LinkedList) append(value int) {
+	node := Node{}
+	node.value = value
+
+	if l.length == 0 {
+		l.head = &node
+		l.length++
+		l.tail = &node
+		return
+	}
+	// ptr - address of the starting/current point through the iteration
+	ptr := l.head
+	for i := 0; i < l.length; i++ {
+		if ptr.next == nil {
+			ptr.next = &node
+			l.length++
+			return
+		}
+		ptr = ptr.next
+	}
+}
