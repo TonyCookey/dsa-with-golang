e80cea54243aca0ded1db1ad823b5e7b2ad6a62f
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-16 12:19:29 +0100

    built a stack using an underlying linked list with the push, pop, isEmpty and peek methods

---
+++ data-structures/stack/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value string
+	next  *Node
+}
+type Stack struct {
+	top    *Node
+	bottom *Node
+	length int
+}
+
+func (s *Stack) peek() string {
+	fmt.Println(s.top.value)
+	return s.top.value
+}
+func (s *Stack) isEmpty() bool {
+	fmt.Println(s.length == 0)
+	return s.length == 0
+}
+func (s *Stack) pop() {
+	s.top = s.top.next
+	s.length--
+	return
+}
+func (s *Stack) push(value string) {
+	newNode := Node{
+		value: value,
+		next:  s.top,
+	}
+	if s.length == 0 {
+		s.bottom = &newNode
+	}
+	s.top = &newNode
+	s.length++
+	return
+}
+
+func main() {
+	stack := Stack{}
+	stack.push("Tony")
+	stack.push("Cookey")
+	stack.push("Starboy")
+	stack.peek()
+	stack.pop()
+	stack.isEmpty()
+	fmt.Println(stack)
+}
