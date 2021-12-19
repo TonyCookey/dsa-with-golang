643014690cf22d8f270c673c3cfebe413bd9ec1d
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-19 19:54:53 +0100

    completed insert method for the Binary Search Tree

--- data-structures/binary-search-tree/main.go
+++ data-structures/binary-search-tree/main.go
@@ -11,7 +11,34 @@ type BinarySearchTree struct {
 
 // inserts values into the binary search tree
 func (b *BinarySearchTree) insert(value int) {
+	newNode := Node{
+		value: value,
+	}
+	if b.root == nil {
+		b.root = &newNode
+		return
+	}
+	//set the current/pointer node
+	currentNode := b.root
+	for currentNode != nil {
+		if currentNode.value > value {
+			if currentNode.right == nil {
+				currentNode.right = &newNode
+				return
+			} else {
+				currentNode = currentNode.right
+			}
 
+		} else {
+			if currentNode.left == nil {
+				currentNode.left = &newNode
+				return
+			} else {
+				currentNode = currentNode.left
+			}
+		}
+	}
+	return
 }
 
 // lookup- finds a node using its value
