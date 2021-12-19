3ebcd25a73356aeac77225c3ecf6fffd7f2ba286
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-19 19:36:19 +0100

    created the types and defined methods for the Binary Search Tree

---
+++ data-structures/binary-search-tree/main.go
@@ -0,0 +1,20 @@
+package main
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+type BinarySearchTree struct {
+	root *Node
+}
+
+// inserts values into the binary search tree
+func (b *BinarySearchTree) insert(value int) {
+
+}
+
+// lookup- finds a node using its value
+func (b *BinarySearchTree) lookup(value int) *Node {
+
+}
