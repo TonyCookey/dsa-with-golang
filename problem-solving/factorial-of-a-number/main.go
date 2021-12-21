31aba0fa85d163a7acf283d38a5870f64bf0bc1f
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-21 13:46:37 +0100

    solved find Factorial using iteration

--- problem-solving/factorial-of-a-number/main.go
+++ problem-solving/factorial-of-a-number/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(findFactorialUsingRecursion(5))
+	fmt.Println(findFactorialUsingIteration(5))
 }
 
 // 5
@@ -12,5 +13,13 @@ func findFactorialUsingRecursion(x int) int {
 		return 1
 	}
 	sum := x * findFactorialUsingRecursion(x-1)
+	return sum
+}
+func findFactorialUsingIteration(x int) int {
+	var sum int = 1
+	for x > 0 {
+		sum *= x
+		x--
+	}
 	return sum
 }
