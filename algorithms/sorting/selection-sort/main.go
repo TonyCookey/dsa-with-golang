0b4215054d2cb0746c7f57ab00e4227647e7e10d
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-23 19:10:17 +0100

    wrote another selection sort algorithm in quadratic time

--- algorithms/sorting/selection-sort/main.go
+++ algorithms/sorting/selection-sort/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	xi := []int{99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0, 23, 20, 18, 28}
 	fmt.Println(selectionSort(xi))
+	fmt.Println(selectionSortSecondApproach(xi))
 }
 
 func selectionSort(xi []int) []int {
@@ -24,6 +25,23 @@ func selectionSort(xi []int) []int {
 			xi[index] = holding
 		}
 		fmt.Println(xi)
+	}
+	return xi
+}
+func selectionSortSecondApproach(xi []int) []int {
+
+	for i := 0; i < len(xi); i++ {
+		minIndex := i
+		for j := i + 1; j < len(xi); j++ {
+			if xi[minIndex] > xi[j] {
+				minIndex = j
+
+			}
+		}
+
+		holding := xi[i]
+		xi[i] = xi[minIndex]
+		xi[minIndex] = holding
 	}
 	return xi
 }
