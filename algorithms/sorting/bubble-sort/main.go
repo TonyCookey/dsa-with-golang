3776d9e7147925da777b77e7dd00cfa26e4ac902
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-23 16:46:52 +0100

    wrote a bubble sort algorithm in quadratic time

---
+++ algorithms/sorting/bubble-sort/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	xi := []int{99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0, 23, 20, 18, 28}
+	fmt.Println(bubbleSort(xi))
+}
+func bubbleSort(xi []int) []int {
+	length := len(xi)
+
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-1; j++ {
+			if xi[j] > xi[j+1] {
+				current := xi[j]
+				xi[j] = xi[j+1]
+				xi[j+1] = current
+			}
+		}
+	}
+	return xi
+}
