0c6cc7277d8601789c87fe65f9d57416e5f70a90
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-31 12:59:05 +0100

    End of Year Repository Cleanup: findFirstRecurringNumber

--- problem-solving/find-first-recurring-number/main.go
+++ problem-solving/find-first-recurring-number/main.go
@@ -6,6 +6,8 @@ func main() {
 	nums := []int{2, 3, 4, 5, 6, 7, 8, 9, 2}
 	fmt.Println(findFirstRecurringNumber(nums))
 }
+
+//findFirstRecurringNumber - find first recurring number using hash tables/maps
 func findFirstRecurringNumber(nums []int) int {
 	uniqueNums := make(map[int]bool)
 
