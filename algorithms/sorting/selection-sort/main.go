972d46b222cc69fda61b19e1e89035ae13dad734
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-31 13:01:17 +0100

    End of Year Repository Cleanup: selection sort algorithm

--- algorithms/sorting/selection-sort/main.go
+++ algorithms/sorting/selection-sort/main.go
@@ -8,8 +8,8 @@ func main() {
 	fmt.Println(selectionSortSecondApproach(xi))
 }
 
+//selectionSort - selection sort algorithm appraoch 1
 func selectionSort(xi []int) []int {
-
 	for i := 0; i < len(xi); i++ {
 		var index int
 		minValue := xi[i]
@@ -28,17 +28,16 @@ func selectionSort(xi []int) []int {
 	}
 	return xi
 }
-func selectionSortSecondApproach(xi []int) []int {
 
+//selectionSortSecondApproach - selection sort algorithm approach 2
+func selectionSortSecondApproach(xi []int) []int {
 	for i := 0; i < len(xi); i++ {
 		minIndex := i
 		for j := i + 1; j < len(xi); j++ {
 			if xi[minIndex] > xi[j] {
 				minIndex = j
-
 			}
 		}
-
 		holding := xi[i]
 		xi[i] = xi[minIndex]
 		xi[minIndex] = holding
