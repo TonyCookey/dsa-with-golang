f732ce6be3858f4aa18104d37c97b6b337399f17
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-31 12:08:46 +0100

    End of Year Repository Cleanup: hash tables

--- data-structures/hash-tables/build-a-hash-table/main.go
+++ data-structures/hash-tables/build-a-hash-table/main.go
@@ -8,6 +8,7 @@ type hashTable struct {
 	data map[int][]string
 }
 
+// hash - hashes the key
 func (h hashTable) hash(key string) int {
 	var hash int
 	for i := 0; i < len(key); i++ {
@@ -15,15 +16,21 @@ func (h hashTable) hash(key string) int {
 	}
 	return hash
 }
+
+// set - sets the key and value
 func (h hashTable) set(key, value string) {
 	hash := h.hash(key)
 	bucket := []string{key, value}
 	h.data[hash] = bucket
 }
+
+// get - returns value using the key
 func (h hashTable) get(key string) string {
 	hash := h.hash(key)
 	return h.data[hash][1]
 }
+
+// keys- returns all the keys in the hashtable
 func (h hashTable) keys() []string {
 	keysSlice := []string{}
 	for _, value := range h.data {
@@ -32,6 +39,7 @@ func (h hashTable) keys() []string {
 	return keysSlice
 }
 
+// values - returns all the values in the hash table
 func (h hashTable) values() []string {
 	keysSlice := []string{}
 	for _, value := range h.data {
