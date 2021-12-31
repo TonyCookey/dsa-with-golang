6bbaff74a8fbc6a368bfc1a05fae855e48997164
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-31 12:57:43 +0100

    End of Year Repository Cleanup: isPalindrome

--- problem-solving/is-palindrome/main.go
+++ problem-solving/is-palindrome/main.go
@@ -10,7 +10,6 @@ func main() {
 }
 
 // isPalindrome - checks to see if a number is a palindrome(reads the same to and fro - from front and back)
-// returns bool value
 func isPalindrome(x int) bool {
 	chars := []rune(strconv.Itoa(x))
 	i := 0
