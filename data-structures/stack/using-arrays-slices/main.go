f909689ef94fe58f7a21e6444f8915a62cbc797d
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-31 12:17:44 +0100

    End of Year Repository Cleanup: Stacks

--- data-structures/stack/using-arrays-slices/main.go
+++ data-structures/stack/using-arrays-slices/main.go
@@ -6,22 +6,28 @@ type Stack struct {
 	data []string
 }
 
+// peek - check the last value in the stack
 func (s *Stack) peek() string {
 	return s.data[len(s.data)-1]
 }
+
+//isEmpty - checks if the stack is empty
 func (s *Stack) isEmpty() bool {
 	fmt.Println(len(s.data) == 0)
 	return len(s.data) == 0
 }
+
+//pop - removes the last value from the stack - LIFO
 func (s *Stack) pop() {
 	if len(s.data) == 0 {
 		return
 	}
-	//s.data = append([]string{}, s.data[:len(s.data) - 1]...)
 	s.data = s.data[:len(s.data)-1]
 	fmt.Println(s.data)
 	return
 }
+
+//push - add to the top of the stack
 func (s *Stack) push(value string) {
 	s.data = append(s.data, value)
 	return
