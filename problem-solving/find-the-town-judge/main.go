f54289dd8db1b4b20a733b03af1d04638d503255
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-01-03 20:47:26 +0100

    problem solving - find a judge final solution - 0(n+m)

--- problem-solving/find-the-town-judge/main.go
+++ problem-solving/find-the-town-judge/main.go
@@ -6,10 +6,54 @@ func main() {
 	//fmt.Println(findJudge(4, [][]int{{1, 3}, {1,4}, {2, 3}, {2,4}, {4,3}}))
 	//fmt.Println(findJudge(1, [][]int{}))
 	//fmt.Println(findJudge(3, [][]int{{1,2},{2,3}}))
+	fmt.Println(findJudge2(3, [][]int{{1, 2}, {2, 3}}))
 	//[[1,2],[1,3],[2,1],[2,3],[1,4],[4,3],[4,1]]
-	fmt.Println(findJudge2(4, [][]int{{1, 2}, {1, 3}, {2, 1}, {2, 3}, {1, 4}, {4, 3}, {4, 1}}))
+	fmt.Println(findJudge(4, [][]int{{1, 2}, {1, 3}, {2, 1}, {2, 3}, {1, 4}, {4, 3}, {4, 1}}))
 	//fmt.Println(findJudge(3, [][]int{{1, 3}, {2,3}, {3, 1}}))
 	//fmt.Println(findJudge(4, [][]int{{1,2},{3,2},{1,3},{4,1},{3,1},{2,1},{2,3},{4,3},{4,2},{3,4},{2,4}}))
+}
+func findJudge(n int, trust [][]int) int {
+	if len(trust) == 0 && n > 1 {
+		return -1
+	}
+	if len(trust) == 0 && n == 1 {
+		return n
+	}
+	//townJudge := trust[0][1]
+	voterMap := map[int]int{trust[0][0]: 1}
+	voteeMap := map[int]int{trust[0][1]: 1}
+
+	for i := 1; i < len(trust); i++ {
+		voter := trust[i][0]
+		votee := trust[i][1]
+		//if count, ok := voteeMap[townJudge]; ok{
+		//	if count != 0 {
+		//		townJudge = voter
+		//	}
+		//}else{
+		//	townJudge = votee
+		//}
+		if _, ok := voterMap[voter]; ok {
+			voterMap[voter]++
+		} else {
+			voterMap[voter] = 1
+		}
+		if _, ok := voteeMap[votee]; ok {
+			voteeMap[votee]++
+		} else {
+			voteeMap[votee] = 1
+		}
+	}
+	for key := range voteeMap {
+		if voteeMap[key] == n-1 && voterMap[key] == 0 {
+			return key
+		}
+	}
+	fmt.Println(voterMap, voteeMap)
+	//if voterMap[townJudge] == 0 && voteeMap[townJudge] == n - 1 {
+	//	return townJudge
+	//}
+	return -1
 }
 func findJudge2(n int, trust [][]int) int {
 	townJudge := trust[0][1]
