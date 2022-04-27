5731bb9cdc393dd5eb3a0bc7e5c6521937192188
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-04-27 13:35:32 +0100

    problem-solving :35. updated Search Insert Position

--- problem-solving/search-index-position/main.go
+++ problem-solving/search-index-position/main.go
@@ -3,11 +3,12 @@ package search_index_position
 func searchInsert(nums []int, target int) int {
 	low, high := 0, len(nums)-1
 	var mid int
-
 	for low <= high {
-		mid = (high + low) / 2
-
-		if nums[mid] < target {
+		// calculate mid point
+		mid = low + ((high - low) / 2)
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
 			low = mid + 1
 		} else {
 			high = mid - 1
