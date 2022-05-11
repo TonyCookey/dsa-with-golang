d5f64b5ff701f445acadd853aae2d72aea914ec1
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-05-11 14:31:55 +0100

    problem-solving: 112. Path Sum

---
+++ problem-solving/path-sum/main.go
@@ -0,0 +1,37 @@
+package path_sum
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	return DFSHelper(root, 0, targetSum)
+}
+
+func DFSHelper(root *TreeNode, sum, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Val+sum == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	sum += root.Val
+
+	left := DFSHelper(root.Left, sum, targetSum)
+	if left {
+		return left
+	}
+
+	right := DFSHelper(root.Right, sum, targetSum)
+	if right {
+		return right
+	}
+	return false
+
+}
