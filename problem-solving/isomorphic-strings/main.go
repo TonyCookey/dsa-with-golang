b9a8a6665e9203056653f9ce003e274fd78ab1a4
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-05-26 15:06:12 +0100

    problem-solving: 205. Isomorphic Strings - updated

--- problem-solving/isomorphic-strings/main.go
+++ problem-solving/isomorphic-strings/main.go
@@ -1,24 +1,20 @@
 package isomorphic_strings
 
 func isIsomorphic(s string, t string) bool {
-	mapping := make(map[byte]byte)
-	for i := 0; i < len(s); i++ {
-		if mapping[s[i]] == 0 {
-			mapping[s[i]] = t[i]
+	forwardMapping := make(map[byte]byte)
+	backwardMapping := make(map[byte]byte)
 
+	for i := 0; i < len(s); i++ {
+		if forwardMapping[s[i]] == 0 {
+			forwardMapping[s[i]] = t[i]
 		}
-		if mapping[s[i]] != 0 && mapping[s[i]] != t[i] {
+		if backwardMapping[t[i]] == 0 {
+			backwardMapping[t[i]] = s[i]
+		}
+		if forwardMapping[s[i]] != 0 && forwardMapping[s[i]] != t[i] {
 			return false
 		}
-	}
-
-	mapping = make(map[byte]byte)
-	for i := 0; i < len(t); i++ {
-		if mapping[t[i]] == 0 {
-			mapping[t[i]] = s[i]
-
-		}
-		if mapping[t[i]] != 0 && mapping[t[i]] != s[i] {
+		if backwardMapping[t[i]] != 0 && backwardMapping[t[i]] != s[i] {
 			return false
 		}
 	}
