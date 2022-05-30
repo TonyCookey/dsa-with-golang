0c240237d2a5b984c394cba0bca3e19eacdc73e8
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-05-30 15:05:16 +0100

    problem-solving: 290. Word Pattern

---
+++ problem-solving/word-pattern/main.go
@@ -0,0 +1,32 @@
+package word_pattern
+
+import "strings"
+
+func wordPattern(pattern string, s string) bool {
+	seenS := make(map[string]byte)
+	seenP := make(map[byte]string)
+
+	ss := strings.Split(s, " ")
+
+	if len(ss) != len(pattern) {
+		return false
+	}
+
+	for i := 0; i < len(pattern); i++ {
+		if seenS[ss[i]] == 0 {
+			seenS[ss[i]] = pattern[i]
+		}
+		if seenS[ss[i]] != 0 && seenS[ss[i]] != pattern[i] {
+			return false
+		}
+	}
+	for i := 0; i < len(pattern); i++ {
+		if seenP[pattern[i]] == "" {
+			seenP[pattern[i]] = ss[i]
+		}
+		if seenP[pattern[i]] != " " && seenP[pattern[i]] != ss[i] {
+			return false
+		}
+	}
+	return true
+}
