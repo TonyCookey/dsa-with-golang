8cdc7d84328e1f2fe29fcdbd263cc80e2d2c2faf
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-06-15 10:25:57 +0100

    problem-solving: 404. Sum of Left Leaves

---
+++ problem-solving/sum-of-left-leaves/main.go
@@ -0,0 +1,17 @@
+package sum_of_left_leaves
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	} else if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		return root.Left.Val + sumOfLeftLeaves(root.Right)
+	} else {
+		return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+	}
+}
