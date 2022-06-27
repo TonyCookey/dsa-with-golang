8a89ecbde1077fc00399ed96599da719d1c2d970
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-06-27 16:40:06 +0100

    problem-solving: 121. Best Time to Buy and Sell Stock updated - Sliding Window

--- problem-solving/best-time-to-buy-and-sell-stock/main.go
+++ problem-solving/best-time-to-buy-and-sell-stock/main.go
@@ -1,22 +1,24 @@
 package best_time_to_buy_and_sell_stock
 
 func maxProfit(prices []int) int {
-	result := []int{0}
-	low, high := prices[0], prices[0]
-	for i := 0; i < len(prices); i++ {
-		if prices[i] < low {
-			if high-low > result[len(result)-1] {
-				result = append(result, high-low)
-			}
-			low = prices[i]
-			high = prices[i]
+	var maxProfit int
+	left, right := 0, 1
+
+	for right < len(prices) {
+		if prices[left] < prices[right] {
+			maxProfit = max(maxProfit, prices[right]-prices[left])
+		} else {
+			left = right
 		}
-		if prices[i] > high {
-			high = prices[i]
-			if high-low > result[len(result)-1] {
-				result = append(result, high-low)
-			}
-		}
+		right++
+
 	}
-	return result[len(result)-1]
+	return maxProfit
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
