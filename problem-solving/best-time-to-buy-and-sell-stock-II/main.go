14a55bd80f7ee512fac3a5a56e68e8810c970bed
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-06-28 18:46:47 +0100

    problem-solving: 122. Best Time to Buy and Sell Stock II

--- problem-solving/best-time-to-buy-and-sell-stock-II/main.go
+++ problem-solving/best-time-to-buy-and-sell-stock-II/main.go
@@ -9,8 +9,19 @@ func maxProfit(prices []int) int {
 		if prices[low] < prices[high] {
 			profit += prices[high] - prices[low]
 		}
-		low = high
+		low++
 		high++
+	}
+	return profit
+
+}
+func maxProfit2(prices []int) int {
+	var profit int
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
 	}
 	return profit
 
