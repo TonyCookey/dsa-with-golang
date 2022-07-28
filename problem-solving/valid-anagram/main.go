0cb783fbdaee08183c728fb7cab00007d0660f1e
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-07-28 12:25:30 +0100

    problem-solving: 242. Valid Anagram

--- problem-solving/valid-anagram/main.go
+++ problem-solving/valid-anagram/main.go
@@ -18,3 +18,21 @@ func isAnagram(s string, t string) bool {
 	}
 	return true
 }
+
+func isAnagram2(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	seen := make(map[byte]int)
+
+	for i, _ := range s {
+		seen[s[i]]++
+		seen[t[i]]--
+	}
+	for _, char := range seen {
+		if char != 0 {
+			return false
+		}
+	}
+	return true
+}
