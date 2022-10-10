0f5d6322fdfa495038a75941dbf0d3416e79c039
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-10-10 20:10:36 +0100

    problem-solving: 2424. Longest Uploaded Prefix

--- problem-solving/longest-uploaded-prefix/main.go
+++ problem-solving/longest-uploaded-prefix/main.go
@@ -1,30 +1,30 @@
 package longest_uploaded_prefix
 
 type LUPrefix struct {
-	arr         []bool
-	last_prefix int
+	arr        []bool
+	lastPrefix int
 }
 
 func Constructor(n int) LUPrefix {
 	return LUPrefix{
-		arr:         make([]bool, n+1, n+1),
-		last_prefix: 1,
+		arr:        make([]bool, n+1, n+1),
+		lastPrefix: 1,
 	}
 }
 
-func (this *LUPrefix) Upload(video int) {
-	this.arr[video] = true
+func (p *LUPrefix) Upload(video int) {
+	p.arr[video] = true
 }
 
-func (this *LUPrefix) Longest() int {
+func (p *LUPrefix) Longest() int {
 
-	for i := this.last_prefix; i < len(this.arr); i++ {
-		if this.arr[i] == false {
+	for i := p.lastPrefix; i < len(p.arr); i++ {
+		if p.arr[i] == false {
 			if i > 1 {
-				this.last_prefix = i - 1
+				p.lastPrefix = i - 1
 			}
 			return i - 1
 		}
 	}
-	return len(this.arr) - 1
+	return len(p.arr) - 1
 }
