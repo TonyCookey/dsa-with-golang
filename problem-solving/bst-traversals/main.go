aebd459372a595748370a4f23c193ee88b46b0ed
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-11-10 14:35:26 +0100

    problem-solving: BST Traversals, Pre, Post, In

---
+++ problem-solving/bst-traversals/main.go
@@ -0,0 +1,48 @@
+package main
+
+type BST struct {
+	Value int
+
+	Left  *BST
+	Right *BST
+}
+
+func (tree *BST) InOrderTraverse(array []int) []int {
+	// Write your code here.
+	if tree.Left != nil {
+		array = tree.Left.InOrderTraverse(array)
+	}
+	array = append(array, tree.Value)
+
+	if tree.Right != nil {
+		array = tree.Right.InOrderTraverse(array)
+	}
+	return array
+}
+
+func (tree *BST) PreOrderTraverse(array []int) []int {
+	// Write your code here.
+	array = append(array, tree.Value)
+
+	if tree.Left != nil {
+		array = tree.Left.PreOrderTraverse(array)
+	}
+
+	if tree.Right != nil {
+		array = tree.Right.PreOrderTraverse(array)
+	}
+	return array
+}
+
+func (tree *BST) PostOrderTraverse(array []int) []int {
+	// Write your code here.
+	if tree.Left != nil {
+		array = tree.Left.PostOrderTraverse(array)
+	}
+	if tree.Right != nil {
+		array = tree.Right.PostOrderTraverse(array)
+	}
+	array = append(array, tree.Value)
+
+	return array
+}
