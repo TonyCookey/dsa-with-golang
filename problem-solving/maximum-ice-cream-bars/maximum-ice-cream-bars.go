9e8e3294e62be131ec47f0119756b1e429211ddc
Anthony Iroegbu <anthony@crenettechlabs.com>
2023-01-02 17:24:02 +0100

    problem-solving: 1833. Maximum Ice Cream Bars

--- problem-solving/maximum-ice-cream-bars/maximum-ice-cream-bars.go
+++ problem-solving/maximum-ice-cream-bars/maximum-ice-cream-bars.go
@@ -1,1 +1,18 @@
 package maximum_ice_cream_bars
+
+import "sort"
+
+func maxIceCream(costs []int, coins int) int {
+	sort.Ints(costs)
+
+	var curr_cost, count int
+
+	for _, cost := range costs {
+		curr_cost += cost
+		if curr_cost > coins {
+			return count
+		}
+		count++
+	}
+	return count
+}
