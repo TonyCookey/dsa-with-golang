b21906adcf2fdefb1a3c942a77c8d81ae47f28ef
Anthony Iroegbu <anthony@crenettechlabs.com>
2023-01-17 12:20:10 +0100

    problem-solving: AE. Minimum Waiting Time

---
+++ problem-solving/minimum-waiting-time/main.go
@@ -0,0 +1,28 @@
+package minimum_waiting_time
+
+import "sort"
+
+// 0(NlogN) Time | 0(1) Space
+func MinimumWaitingTime(queries []int) int {
+	// Write your code here
+	sort.Ints(queries)
+	waitingTime := 0
+	sum := 0
+	for i := 1; i < len(queries); i++ {
+		waitingTime += queries[i-1]
+		sum += waitingTime
+	}
+	return sum
+}
+
+func MinimumWaitingTimeOptimal(queries []int) int {
+	// Write your code here
+	sort.Ints(queries)
+	waitingTime := 0
+
+	for idx, duration := range queries {
+		queriesLeft := len(queries) - (idx + 1)
+		waitingTime += duration * queriesLeft
+	}
+	return waitingTime
+}
