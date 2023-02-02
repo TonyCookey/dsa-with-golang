ee8dfa9fd85888bd3cbda9c3e5fba6c02f68fecd
Anthony Iroegbu <anthony@crenettechlabs.com>
2023-02-02 12:47:16 +0100

    problem-solving: AE. Is Palindrome

--- problem-solving/is-palindrome/main.go
+++ problem-solving/is-palindrome/main.go
@@ -28,7 +28,7 @@ func isPalindrome(x int) bool {
 func IsPalindrome(str string) bool {
 	start := 0
 	end := len(str) - 1
-	for start <= end {
+	for start < end {
 		if str[start] != str[end] {
 			return false
 		}
