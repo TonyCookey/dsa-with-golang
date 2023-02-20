c431a7db4d333ebf24e0d415de49a98da45fe62e
Anthony Iroegbu <anthony@crenettechlabs.com>
2023-02-20 11:55:35 +0100

    problem-solving: AE. Monotonic Array cleanup

--- problem-solving/monotonic-array/main.go
+++ problem-solving/monotonic-array/main.go
@@ -4,16 +4,13 @@ func IsMonotonic(array []int) bool {
 	NonIncreasing, NonDecreasing := true, true
 
 	for i := 1; i < len(array); i++ {
-		if NonIncreasing && array[i] > array[i-1] {
+		if array[i] > array[i-1] {
 			NonIncreasing = false
 		}
 
-		if NonDecreasing && array[i] < array[i-1] {
+		if array[i] < array[i-1] {
 			NonDecreasing = false
 		}
 	}
-	if NonIncreasing || NonDecreasing {
-		return true
-	}
-	return false
+	return NonIncreasing || NonDecreasing
 }
