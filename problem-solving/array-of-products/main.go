e43690cfe8c02b5e0b2fb6fe31f8efbf8fd2faea
Anthony Iroegbu <anthony@crenettechlabs.com>
2023-02-23 17:25:36 +0100

    problem-solving: AE. Array of Products

--- problem-solving/array-of-products/main.go
+++ problem-solving/array-of-products/main.go
@@ -1,5 +1,6 @@
 package main
 
+//ArrayOfProducts - 0(N) Time | 0(N) Space
 func ArrayOfProducts(array []int) []int {
 	var result []int
 	cumulativeSum := 1
@@ -20,5 +21,23 @@ func ArrayOfProducts(array []int) []int {
 		cumulativeSum *= array[idx]
 	}
 	result = append(result, cumulativeSum)
+	return result
+}
+
+//ArrayOfProductsBruteForce - 0(N^2) Time | 0(N) Space
+func ArrayOfProductsBruteForce(array []int) []int {
+	// Write your code here.
+	var result []int
+	for i := 0; i < len(array); i++ {
+		sum := 1
+		for left := i - 1; left >= 0; left-- {
+			sum *= array[left]
+		}
+		for right := i + 1; right < len(array); right++ {
+			sum *= array[right]
+		}
+		result = append(result, sum)
+	}
+
 	return result
 }
